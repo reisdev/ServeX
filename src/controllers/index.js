927ad64c0db14179dcc8b6c5cfbf096bd5b36ec1
MatheusDosReis <matheusdrdj@gmail.com>
2017-11-23 14:34:17 -0200

    Nome errado, corrigido

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,5 @@
 /*! @Author: Raphael Nepomuceno <raphael.nepomuceno@ufv.br> */
 
-export * from './serviceCat.js'
+export * from './serviceCategory.js'
 export * from './services.js'
 export * from './user.js'
